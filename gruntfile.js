bb20e6009734a1c1bea3e903183031fe2ef94508
Matthijs <matthijs0703@hotmail.com>
2018-10-02 16:56:43 +0200

    first version

---
+++ gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function(grunt) {
+	// Add configuration, tasks and plugins here
+	grunt.initConfig({
+
+		// imports the config data from the package.json
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				files: {
+					'css/style.css': 'sass/style.scss'
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['sass/**'],
+				tasks: ['sass','cssnano']
+			}
+		},
+		cssnano: {
+			dist: {
+				files: {
+					'css/style.min.css': 'css/style.css'
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-cssnano');
+	grunt.registerTask('default', 'watch')
+};
---
+++ javascript/projects-max5.js
@@ -0,0 +1,27 @@
+const projects = {
+	ods: { name: "Old Dance School" , developer: "FULL-STACK DEVELOPER", img: "../images/Matthijs_About.jpg" , url: "projects/ODSDetails.html"},
+	ftsfWeb: { name: "Fair Trade Software Foundation Webshop" , developer: "FULL-STACK DEVELOPER", img: "../images/doggo.jpg" , url: "projects/FTSFWebDetails.html"},
+	ftsfCMS: { name: "Fair Trade Software Foundation CMS" , developer: "FULL-STACK DEVELOPER", img: "../images/Matthijs_About.jpg" , url: "projects/FTSFCMSDetails.html"},
+	smoel: {name: "Smoelenboek" , developer: "FULL-STACK DEVELOPER" , img: "../images/doggo.jpg" , url: "projects/SmoelDetails.html"},
+	ODSAdmin: {name: "Old dance School Admin" , developer: "FULL-STACK DEVELOPER" , img: "../images/Matthijs_About.jpg" , url: "projects/SmoelDetails.html"}
+};
+
+const PROJECT_ITEM = document.querySelectorAll('.project__projectname');
+
+
+const HEADER = document.querySelector('.project__title');
+const DEVELOPER = document.querySelector('.project__developer');
+const LINK = document.querySelector('.porject__link');
+const IMG = document.querySelector('.project__projectimg');
+
+
+PROJECT_ITEM.forEach(item => {
+	item.addEventListener('click', () => {
+		let projectInfor = projects[item.dataset.name];
+
+		HEADER.innerHTML = projectInfor.name;
+		DEVELOPER.innerHTML = projectInfor.developer;
+		LINK.href = projectInfor.url;
+		IMG.src = projectInfor.img;
+	});
+});
